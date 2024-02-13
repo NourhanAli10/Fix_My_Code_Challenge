516243b7652d0761689defd8b1724b9f94a75cb9
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-13 02:25:58 +0200

    tasks

--- 0x01-challenge/status_server/api/v1/views.py
+++
@@ -1,9 +0,0 @@
-# api/v1/views.py
-
-from flask import Blueprint, jsonify
-
-app_views = Blueprint('app_views', __name__)
-
-@app_views.route('/api/v1/status', methods=['GET'])
-def get_status():
-    return jsonify({"status": "API is up and running"})
